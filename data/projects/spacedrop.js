0b6c67eb2084d887c5abbb74867e691bc5f4dd54
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2019-08-12 19:58:35 +0300

    Update stories

--- data/projects/spacedrop.js
+++ data/projects/spacedrop.js
@@ -1,9 +1,12 @@
-import projects from '../projects'
 import Project from '../../lib/Project'
+import { storyFromMarkdown } from '../helpers'
 
 const spacedrop = new Project({
   uid: 'spacedrop',
   title: 'Spacedrop',
+  stories: [
+    storyFromMarkdown()
+  ]
 })
 
 export default spacedrop
