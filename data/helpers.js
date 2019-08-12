d687d30d584a445a56c9d602e61c83135035c3b5
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2019-08-12 17:44:27 +0300

    Implement storyFromMarkdown

--- data/helpers.js
+++ data/helpers.js
@@ -1,19 +1,52 @@
 import cheerio from 'cheerio'
-import MarkdownIt from'markdown-it'
-import Story from "../lib/Story"
+import MarkdownIt from 'markdown-it'
+import Story from '../lib/Story'
 
 export const markdown = new MarkdownIt({
   typographer: false,
 })
 
-export function storyFromMarkdown(text) {
-  const $document = cheerio.load(markdown.render(text.trim()))
-  return new Story(
-
-  )
+const cheerioOptions = {
+  withDomLvl1: true,
+  normalizeWhitespace: false,
+  xmlMode: true,
+  decodeEntities: true,
 }
 
-export function story(title, events) {
+export function storyFromMarkdown (text, opts) {
+  const options = Object.assign({
+    title: '',
+    content: ''
+    // description: '',
+    // events: [],
+    // notes: [],
+  }, opts)
+  const $ = cheerio.load(markdown.render(text.trim(), { linkify: true }).trim(), cheerioOptions)
+  const nodes = $.root().children()
+  for (let i = 0; i < nodes.length; i++) {
+    const child = nodes[i]
+    if (child.tagName === 'h3' && !options.title) {
+      options.title = $(child).text()
+    }
+    // if (child.tagName === 'p' && !options.description) {
+    //
+    // }
+    // if (child.tagName === 'ul' && !options.events.length) {
+    //   const children = $(child).children()
+    //   for (let j = 0; j < children.length; j++) {
+    //     options.events.push()
+    //   }
+    // }
+  }
+  options.content = $.xml()
+  return new Story(options)
+}
+
+export function normalizeHTML(html) {
+  return cheerio.load(html, cheerioOptions).xml().replace(/^\s+/gm, '')
+}
+
+export function story (title, events) {
   return new Story({
     actor: title[0],
     plot: title[1],
--- data/helpers.test.js
+++ data/helpers.test.js
@@ -1,14 +1,70 @@
-import Story from "../lib/Story"
-import {storyFromMarkdown} from "./helpers"
+import Story from '../lib/Story'
+import Event from '../lib/Event'
+import Person from '../lib/Actor/Person'
+import { storyFromMarkdown, normalizeHTML } from './helpers'
 
 it('parses story', () => {
+  const Alice = new Person({
+    title: 'Alice'
+  })
   expect(storyFromMarkdown(`
   ### Alice buys cryptocurrency
   
   * Alice buys cryptocurrency via regular crypto exchange if she already has BTC, ETH or USDT
   * Alice buys cryptocurrency via peer-to-peer crypto exchange if there are sellers nearby
   * Alice buys cryptocurrency via credit card crypto exchange
-`)).toBe(new Story(
-
-  ));
-});
+    * Options:
+      * https://www.coinmama.com/
+  
+  Notes:
+  * Some note here
+`, { actor: Alice })).toEqual(new Story({
+    actor: Alice,
+    title: 'Alice buys cryptocurrency',
+    content: normalizeHTML(`
+      <h3>Alice buys cryptocurrency</h3>
+      <ul>
+      <li>Alice buys cryptocurrency via regular crypto exchange if she already has BTC, ETH or USDT</li>
+      <li>Alice buys cryptocurrency via peer-to-peer crypto exchange if there are sellers nearby</li>
+      <li>Alice buys cryptocurrency via credit card crypto exchange
+      <ul>
+      <li>Options:
+      <ul>
+      <li>https://www.coinmama.com/</li>
+      </ul>
+      </li>
+      </ul>
+      </li>
+      </ul>
+      <p>Notes:</p>
+      <ul>
+      <li>Some note here</li>
+      </ul>
+    `.trim()),
+    // events: [
+    //   new Event({
+    //     title: 'Alice buys cryptocurrency via regular crypto exchange if she already has BTC, ETH or USDT',
+    //     actor: Alice,
+    //   }),
+    //   new Event({
+    //     title: 'Alice buys cryptocurrency via peer-to-peer crypto exchange if there are sellers nearby',
+    //     actor: Alice,
+    //   }),
+    //   new Event({
+    //     title: 'Alice buys cryptocurrency via credit card crypto exchange',
+    //     actor: Alice,
+    //     extras: [
+    //       {
+    //         title: 'Options',
+    //         points: [
+    //           'https://www.coinmama.com/'
+    //         ]
+    //       }
+    //     ]
+    //   }),
+    // ],
+    // notes: [
+    //   'Some note here'
+    // ]
+  }))
+})
