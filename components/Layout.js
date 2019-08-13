150a78825af6d90879eda5987a8147b83507fd51
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2019-08-13 13:06:07 +0300

    Add static assets

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
 import 'semantic-ui-css/semantic.min.css'
 import './Layout.css'
+import Head from 'next/head'
 import {Container} from "semantic-ui-react"
 import NavBar from './NavBar'
 
 export default function Layout(props) {
   return (
     <div>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/static/favicon.ico' />
+      </Head>
       <NavBar/>
       <Container className="main" text>
         {props.children}
